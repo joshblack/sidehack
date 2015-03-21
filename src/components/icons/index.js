ae5d9c22fe54bdf58d0f5923bdab054844aba848
Josh Black <joshuatblack@ufl.edu>
2015-03-21 18:56:48 -0400

    Adding Icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { GitHubIcon } from './GitHubIcon';
+
+export const Icons = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Hello icons</h1>
+        <GitHubIcon />
+      </div>
+    );
+  }
+});
---
+++ src/components/icons/styles.js
@@ -0,0 +1,5 @@
+import { css } from '../../helpers';
+
+export const spacing = css`
+  margin: 10px;
+`;
